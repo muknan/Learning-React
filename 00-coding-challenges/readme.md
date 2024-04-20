0fa6ffa9f14aceaed16b5ded9e951a3f69a78fd4
muknan <muk.msi@gmail.com>
2024-04-20 12:28:52 +0530

    coding challenge

---
+++ 00-coding-challenges/readme.md
@@ -0,0 +1,3 @@
+<!-- Coding Challenge 1 -->
+
+[text](https://codesandbox.io/p/sandbox/recursing-goodall-m54cjj?file=%2Fsrc%2Findex.js%3A47%2C24)https://codesandbox.io/p/sandbox/recursing-goodall-m54cjj?file=%2Fsrc%2Findex.js%3A47%2C24
--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 // import ReactDOM from "react-dom"; // import before v18
 
 const pizzaData = [
@@ -49,23 +50,78 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
-      <h1>Hello React!</h1>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
 
-function Pizza() {
+function Header() {
+  // const style = {
+  //   color: "red",
+  //   fontSize: "36px",
+  //   textTransform: "uppercase",
+  // };
+  const style = {};
+
   return (
-    <div>
-      <img src="pizzas/funghi.jpg" alt="Pizza Funghi" />
-      <h2>Pizza Funghi</h2>
-      <p>Tomato, mozarella, mushrooms, and onion</p>
+    <header className="header">
+      <h1 style={style}>Nova Scotia Pizzeria</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <Pizza
+        name="Pizza Prosciutto"
+        ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+        imgURL="pizzas/prosciutto.jpg"
+        price={18}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        imgURL="pizzas/funghi.jpg"
+        price={10}
+      />
+    </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.imgURL} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
     </div>
   );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHours = 12;
+  const closeHours = 22;
+  const isOpen = hour >= openHours && hour <= closeHours;
+
+  // if (hour >= openHours && hour <= closeHours) alert("We are currently open!");
+  // else alert("Sorry we are closed");
+
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()} We're currently
+      {isOpen ? " open!" : " closed!"}
+    </footer>
+  );
+  // return React.createElement("footer", null, "We're currently open!");
 }
 
 // React v18
