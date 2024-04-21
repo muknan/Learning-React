d6c1e2d2ea655d1b9f6d6e2ff65d98f591e8856d
muknan <muk.msi@gmail.com>
2024-04-21 18:08:33 +0530

    Updating state based on current state

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -11,13 +11,13 @@ export default function App() {
   const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
   }
   function handleClose() {
-    setIsOpen(!isOpen);
+    setIsOpen((is) => !is);
     setStep(1);
   }
 
