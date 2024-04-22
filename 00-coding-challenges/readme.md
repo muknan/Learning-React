0c987fac5be8a090b044d8a0fd6596172a695f36
muknan <muk.msi@gmail.com>
2024-04-22 18:01:55 +0530

    challenge 2, new project - layout, form submissions

--- 00-coding-challenges/readme.md
+++ 00-coding-challenges/readme.md
@@ -1,3 +1,4 @@
 ### Coding Challenge 1
 
 [Link](https://codesandbox.io/p/sandbox/recursing-goodall-m54cjj?file=%2Fsrc%2Findex.js%3A47%2C24)
+[Link](https://codesandbox.io/p/sandbox/sweet-fast-qx4l5m?file=%2Fsrc%2FApp.js%3A17%2C1)
---
+++ 05-travel-list/src/App.js
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+
+function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your ✈️ trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((n) => (
+          <option value={n} key={n}>
+            {n}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item... "></input>
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>👜 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
