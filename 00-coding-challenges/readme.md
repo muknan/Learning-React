133278d868a04edc9eb3e24dafc67dfab464bed0
muknan <muk.msi@gmail.com>
2024-04-23 15:34:13 +0530

    Lifting state up

--- 00-coding-challenges/readme.md
+++ 00-coding-challenges/readme.md
@@ -1,4 +1,15 @@
-### Coding Challenge 1
+#### Coding Challenge 1
 
 [Link](https://codesandbox.io/p/sandbox/recursing-goodall-m54cjj?file=%2Fsrc%2Findex.js%3A47%2C24)
+
+#### Coding Challenge 2
+
 [Link](https://codesandbox.io/p/sandbox/sweet-fast-qx4l5m?file=%2Fsrc%2FApp.js%3A17%2C1)
+
+#### Exercise 1
+
+[Link](https://codesandbox.io/p/sandbox/react-exercise-flashcards-starter-forked-dpd6fh?file=%2Fsrc%2FApp.js%3A53%2C75)
+
+#### Coding Challenge 3
+
+[Link](https://codesandbox.io/p/sandbox/musing-sun-x77yn9?file=%2Fsrc%2FApp.js%3A64%2C26)
--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,17 +1,23 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: true },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: false },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: true },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+//   { id: 3, description: "Charger", quantity: 1, packed: false },
+// ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -21,7 +27,7 @@ function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -37,8 +43,7 @@ function Form() {
       packed: false,
     };
 
-    initialItems.push(newItem);
-    console.log(initialItems);
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -65,11 +70,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
