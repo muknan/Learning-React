ddaa306c68a3fa5ffdadebdd84dd89a093c1ed0b
muknan <muk.msi@gmail.com>
2024-05-06 21:31:31 +0530

    index and nested routes

--- 11-worldwise/src/pages/AppLayout.jsx
+++ 11-worldwise/src/pages/AppLayout.jsx
@@ -1,10 +1,11 @@
-import AppNav from "../components/AppNav";
-
+import styles from "./AppLayout.module.css";
+import Sidebar from "../components/Sidebar";
+import Map from "../components/Map";
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
-      <p>App layout</p>
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }
