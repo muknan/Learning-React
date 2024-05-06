b10e968ad8cc1565c24dd04c3ab5a02c53ef8cda
muknan <muk.msi@gmail.com>
2024-05-06 18:10:38 +0530

    linking routes and styles with css modules

--- 11-worldwise/src/components/PageNav.jsx
+++ 11-worldwise/src/components/PageNav.jsx
@@ -1,19 +1,24 @@
 import { NavLink } from "react-router-dom";
+import styles from "./PageNav.module.css";
+import Logo from "./Logo";
 
 // NavLink adds an "active" class to active/current URL, which can be styled using CSS
 
 function PageNav() {
   return (
-    <nav>
+    <nav className={styles.nav}>
+      <Logo />
       <ul>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
-          <NavLink to="pricing">Pricing</NavLink>
+          <NavLink to="/product">Product</NavLink>
         </li>
         <li>
-          <NavLink to="product">Product</NavLink>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
--- 11-worldwise/src/pages/PageNotFound.jsx
+++ 11-worldwise/src/pages/PageNotFound.jsx
@@ -1,12 +1,7 @@
-import PageNav from "../components/PageNav";
-
-function PageNotFound() {
+export default function PageNotFound() {
   return (
     <div>
-      <PageNav />
-      <h1>Page not Found! 😟</h1>
+      <h1>Page not found 😢</h1>
     </div>
   );
 }
-
-export default PageNotFound;
