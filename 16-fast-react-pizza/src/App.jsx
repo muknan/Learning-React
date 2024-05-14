af522a49154302705f4fe6db9edba25681356eca
muknan <muk.msi@gmail.com>
2024-05-14 14:13:34 +0530

    fetching data using loader in react router

--- 16-fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/src/App.jsx
@@ -1,6 +1,28 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateUser from "./features/user/CreateUser";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/menu", element: <Menu />, loader: menuLoader },
+      { path: "/cart", element: <Cart /> },
+      { path: "/user", element: <CreateUser /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orderId", element: <Order /> },
+    ],
+  },
+]);
+
 function App() {
-  const x = 10;
-  return <div>Hello World!</div>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
